9f99f84d148bc14af54264a5e18888ab4e5dbe55
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-08 22:39:45 -0400

    Add Mongoose module

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
 
+// Connect to the database with Mongoose
+import './config/database.js'
+
 // import routers
 import { router as indexRouter } from './routes/index.js'
 import { router as skillsRouter } from './routes/skills.js'
