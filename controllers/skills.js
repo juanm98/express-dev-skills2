30abae78679f5fd9af84c5f42a7f9e678d2a3344
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-08 23:08:52 -0400

    Part 2 done

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,8 +1,15 @@
-import { skills } from '../data/skill-data.js'
+import { Skill } from "../models/skill.js"
 
 function index(req, res) {
-  res.render('skills/index', {
-    skills: skills
+  Skill.find({})
+  .then(skills => { // skills represents the result of the query, in this case ALL skills
+    res.render('skills/index', {
+      skills: skills,
+    })
+  })
+  .catch(error => { // If there's an error, console.log it and redirect back home!
+    console.log(error)
+    res.redirect('/')
   })
 }
 
